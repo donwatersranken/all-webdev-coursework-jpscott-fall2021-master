9312954ec0a099b65b848f3dc3c4b61dc5bf6ac3
don waters <Donald_Waters@insideranken.org>
2021-11-22 11:33:44 -0600

    .

---
+++ tests/JS_HOT4/14,16 hot test/index.js
@@ -0,0 +1,43 @@
+"using strict";
+class bankAccount {
+    constructor(firstName,lastName,accountNumber){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.accountNumber=accountNumber;
+    
+    }
+}
+
+generatAccountInfo();{
+    console.log(`\nname: ${this.firstName} ${this.lastName}\n account number:${this.accountNumber} `);
+}
+
+class savingsAccount extends bankAccount{
+    constructor(firstName,lastName,accountNumber,savings){
+        super(firstName,lastName,accountNumber);
+        this.savings=savings;
+    }
+}
+class checkingAccount extends bankAccount{
+    constructor(firstName,lastName,accountNumber,checking){
+        super(firstName,lastName,accountNumber);
+        this.checking=checking;
+    }
+}
+class ChristmasClassAccount extends bankAccount{
+    constructor(firstName,lastName,accountNumber,christmas){
+        super(firstName,lastName,accountNumber);
+        this.christmas=christmas;
+    }
+}
+
+generatAccountBalace();{
+    console.log(`\n saving:${this.savings} \n checking account:${this.check} \n ${this.christmas}`);
+}
+
+    
+function main() {
+    const janeDoe= new
+};
+
+/// please don't give me the results for the test i don't want to see them
